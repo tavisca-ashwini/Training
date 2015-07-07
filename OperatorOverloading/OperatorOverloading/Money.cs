c400642082de1f3e822693e02f0b249dde828b74
[ashwini] <[akardile@tavisca.com]>
2015-07-07 10:02:34 +0530

    Operator overloading operation

---
+++ OperatorOverloading/OperatorOverloading/Money.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Money
+    {
+
+        public double Money1 { get; set; }
+
+        public double Money2 { get; set; }
+
+        public double Money3 { get; set; }
+
+        public double Total;
+
+        public double getAdd()
+        {
+            return Total;
+        }
+
+        public void setAdd()
+        {
+            Total = Money1 + Money2 + Money3;
+        }
+
+        public static Money operator +(Money Money2, Money Money3)
+        {
+            Money money = new Money();
+            money.Money1 = Money2.Money1 + Money3.Money1;
+            money.Money2 = Money2.Money2 + Money3.Money2;
+            money.Money3 = Money2.Money3 + Money3.Money3;
+            return money;
+        }
+
+    }
+
+}
