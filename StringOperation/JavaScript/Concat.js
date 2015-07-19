811f5c3401dfaedf4a7054cb7fa6b550b750a757
Ashwini <akardile@tavisca.com>
2015-07-20 00:33:10 +0530

    Code for string operation is added

---
+++ StringOperation/JavaScript/Concat.js
@@ -0,0 +1,8 @@
+﻿String.prototype.Concat = function (stringTwo) {
+    var stringOne = this;
+    var stringResult = stringOne;
+    for (i = 0; i < stringTwo.length; i++) {
+        stringResult += stringTwo.charAt(i);
+    }
+    return stringResult;
+};
