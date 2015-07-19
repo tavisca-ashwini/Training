c5745db5057d240d498eb15d9ef34dd038ccd0d1
Ashwini <akardile@tavisca.com>
2015-07-19 21:28:21 +0530

    code for bouncing ball is added

---
+++ BouncingBallApplication/JavaScript/BouncingBall.js
@@ -0,0 +1,48 @@
+﻿var step = 5;
+var timeout = 10;
+var height = 0;
+var width = 0;
+var yFlag = false;
+var xFlag = false;
+var interval;
+var xPosition = 20;
+var yPosition = 20;
+
+function moveBall() {
+    height = window.innerHeight - 100;
+    width = window.innerWidth - 100;
+    document.getElementById('ball').style.top = yPosition;
+    document.getElementById('ball').style.left = xPosition;
+    if (yFlag) {
+        yPosition = yPosition + step;
+    }
+    else {
+        yPosition = yPosition - step;
+    }
+    if (yPosition < 0) {
+        yFlag = true;
+        yPosition = 0;
+    }
+    if (yPosition >= (height)) {
+        yFlag = false;
+        yPosition = (height);
+    }
+    if (xFlag) {
+        xPosition = xPosition + step;
+    }
+    else {
+        xPosition = xPosition - step;
+    }
+    if (xPosition < 0) {
+        xFlag = true;
+        xPos = 0;
+    }
+    if (xPosition >= (width)) {
+        xFlag = false;
+        xPosition = (width);
+    }
+}
+function start() {
+    interval = setInterval(moveBall, timeout);
+}
+start();
