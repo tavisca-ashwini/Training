57339114f5a2dadc9b4f3e5e556b3a63477ceb53
Ashwini <akardile@tavisca.com>
2015-07-20 23:23:00 +0530

    code for bouncing ball is added

---
+++ BouncingBallApplication/JavaScript/BouncingBall.js
@@ -0,0 +1,29 @@
+﻿window.BouncingBall = window.BouncingBall || {
+    yFlag: false,
+    xFlag: false,
+    xPosition: 20,
+    yPosition: 20,
+}
+var step = 5;
+
+window.BouncingBall.moveBall = function () {
+
+    document.getElementById('ball').style.top = window.BouncingBall.yPosition;
+    document.getElementById('ball').style.left = window.BouncingBall.xPosition;
+
+    window.BouncingBall.boundryCondition();
+    if (window.BouncingBall.yFlag) {
+        window.BouncingBall.yPosition = window.BouncingBall.yPosition + step;
+    }
+    else {
+        window.BouncingBall.yPosition = window.BouncingBall.yPosition - step;
+    }
+    if (window.BouncingBall.xFlag) {
+        window.BouncingBall.xPosition = window.BouncingBall.xPosition + step;
+    }
+    else {
+        window.BouncingBall.xPosition = window.BouncingBall.xPosition - step;
+    }
+}window.BouncingBall.start = function () {
+    setInterval(BouncingBall.moveBall, 20);
+}
---
+++ BouncingBallApplication/JavaScript/Container.js
@@ -0,0 +1,25 @@
+﻿window.BouncingBall = window.BouncingBall || {}
+
+window.BouncingBall.boundryCondition = function () {
+
+     Bouheight = window.innerHeight - 100;
+    var width = window.innerWidth - 100;
+
+    if (window.BouncingBall.yPosition < 0) {
+        window.BouncingBall.yFlag = true;
+        window.BouncingBall.yPosition = 0;
+    }
+    if (window.BouncingBall.yPosition >= (height)) {
+        window.BouncingBall.yFlag = false;
+        window.BouncingBall.yPosition = (height);
+    }
+
+    if (window.BouncingBall.xPosition < 0) {
+        window.BouncingBall.xFlag = true;
+        window.BouncingBall.xPos = 0;
+    }
+    if (window.BouncingBall.xPosition >= (width)) {
+        window.BouncingBall.xFlag = false;
+        window.BouncingBall.xPosition = (width);
+    }
+}
