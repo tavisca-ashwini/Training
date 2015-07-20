9f686ef81f799cf6ee19334fa831cd6a5e956935
Ashwini <akardile@tavisca.com>
2015-07-20 15:47:35 +0530

    updated code for string opeartion

---
+++ StringManipulation (2)/SubString/SubString.js
@@ -0,0 +1,17 @@
+﻿String.prototype.SubString = function (startIndex, endIndex) {
+    var string = this;
+    if (startIndex >= string.length || startIndex < 0) {
+        throw new TypeError;
+        return;
+    }
+    if (endIndex >= string.length || endIndex < startIndex) {
+        throw new TypeError;
+        return;
+    }
+    var result = "";
+    for (i = startIndex; i < endIndex; i++) {
+
+        result += string.charAt(i);
+    }
+    return result;
+};
