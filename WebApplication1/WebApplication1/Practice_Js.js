072a4d5afb2dbc927b16061b892355553abbd6dd
unknown <akardile@AshwiniK.internal.tavisca.com>
2015-09-03 11:04:51 +0530

    Updated code

---
+++ WebApplication1/WebApplication1/Practice_Js.js
@@ -0,0 +1,21 @@
+﻿var MyGame = function(user) {
+    user = prompt("What would you like to play ?", "Enter Here").toUpperCase();
+    //var player1, player2;
+    switch(user)
+    {
+        case 'Tic-Tac-Toe' :
+            document.write("Selected Tic-Tac-Toe");
+            break;
+
+        case 'Chess':
+            document.write("Selected Chess");
+            break;
+
+        case 'CS':
+            document.write("Selected CS");
+            break;
+
+        default:
+            document.write("Game is not included");
+    }
+};
